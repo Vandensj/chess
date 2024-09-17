dac1b948bde688260a012fa2203086bf61e4a36f
root <root@LAPTOP-EFPRBT9A>
2024-09-17 17:52:59 -0600

    pawn working except for promotion

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -13,17 +13,18 @@ public class PawnMovesCalculator extends MovesCalculator {
         if (pieceColor == ChessGame.TeamColor.BLACK) {
             // Check if piece can go down one
             ChessPosition downOne = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn());
-            if (board.getPiece(downOne) != null && !downOne.outsideBounds()) {
+            if (board.getPiece(downOne) == null && !downOne.outsideBounds()) {
                 //Check if piece can be promoted
                 if (downOne.getRow() == 1)
                     validMoves.add(new ChessMove(myPosition, downOne, null));
-                else
+                else {
                     validMoves.add(new ChessMove(myPosition, downOne, null));
+                    // Check if piece can go down two
+                    ChessPosition downTwo = new ChessPosition(myPosition.getRow() - 2, myPosition.getColumn());
+                    if (myPosition.getRow() == 7 && board.getPiece(downTwo) == null)
+                        validMoves.add(new ChessMove(myPosition, downTwo, null));
+                }
             }
-            // Check if piece can go down two
-            ChessPosition downTwo = new ChessPosition(myPosition.getRow()-2, myPosition.getColumn());
-            if (myPosition.getRow() == 7 && board.getPiece(downOne) != null && !downOne.outsideBounds())
-                validMoves.add(new ChessMove(myPosition, downTwo, null));
             // Check diagonal moves
             ChessPosition downRight = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()+1);
             if (!downRight.outsideBounds() && board.getPiece(downRight) != null && board.getPiece(downRight).getTeamColor() != pieceColor)
@@ -31,12 +32,29 @@ public class PawnMovesCalculator extends MovesCalculator {
             ChessPosition downLeft = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()-1);
             if (!downRight.outsideBounds() && board.getPiece(downLeft) != null && board.getPiece(downLeft).getTeamColor() != pieceColor)
                 validMoves.add(new ChessMove(myPosition, downLeft, null));
+        } else {
+            // Check if piece can go up one
+            ChessPosition upOne = new ChessPosition(myPosition.getRow()+1, myPosition.getColumn());
+            if (board.getPiece(upOne) == null && !upOne.outsideBounds()) {
+                //Check if piece can be promoted
+                if (upOne.getRow() == 1)
+                    validMoves.add(new ChessMove(myPosition, upOne, null));
+                else {
+                    validMoves.add(new ChessMove(myPosition, upOne, null));
+                    // Check if piece can go up two
+                    ChessPosition upTwo = new ChessPosition(myPosition.getRow() + 2, myPosition.getColumn());
+                    if (myPosition.getRow() == 2 && board.getPiece(upTwo) == null)
+                        validMoves.add(new ChessMove(myPosition, upTwo, null));
+                }
+            }
+            // Check diagonal moves
+            ChessPosition upRight = new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()+1);
+            if (!upRight.outsideBounds() && board.getPiece(upRight) != null && board.getPiece(upRight).getTeamColor() != pieceColor)
+                validMoves.add(new ChessMove(myPosition, upRight, null));
+            ChessPosition upLeft = new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()-1);
+            if (!upRight.outsideBounds() && board.getPiece(upLeft) != null && board.getPiece(upLeft).getTeamColor() != pieceColor)
+                validMoves.add(new ChessMove(myPosition, upLeft, null));
         }
-
-        // Determine move direction based on the pawn's color
-        int direction = (pieceColor == ChessGame.TeamColor.WHITE) ? 1 : -1;
-
-        // One step forward
 
         return validMoves;
     }
