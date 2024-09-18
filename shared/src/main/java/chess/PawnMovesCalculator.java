1e3223c291c5c183601e321476abae5dfc416e1e
root <root@LAPTOP-EFPRBT9A>
2024-09-17 18:35:04 -0600

    pawn promotion working

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -15,9 +15,13 @@ public class PawnMovesCalculator extends MovesCalculator {
             ChessPosition downOne = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn());
             if (board.getPiece(downOne) == null && !downOne.outsideBounds()) {
                 //Check if piece can be promoted
-                if (downOne.getRow() == 1)
-                    validMoves.add(new ChessMove(myPosition, downOne, null));
-                else {
+                if (downOne.getRow() == 1) {
+                    // If yes return all possible promotions
+                    validMoves.add(new ChessMove(myPosition, downOne, ChessPiece.PieceType.BISHOP));
+                    validMoves.add(new ChessMove(myPosition, downOne, ChessPiece.PieceType.KNIGHT));
+                    validMoves.add(new ChessMove(myPosition, downOne, ChessPiece.PieceType.QUEEN));
+                    validMoves.add(new ChessMove(myPosition, downOne, ChessPiece.PieceType.ROOK));
+                } else {
                     validMoves.add(new ChessMove(myPosition, downOne, null));
                     // Check if piece can go down two
                     ChessPosition downTwo = new ChessPosition(myPosition.getRow() - 2, myPosition.getColumn());
@@ -27,21 +31,39 @@ public class PawnMovesCalculator extends MovesCalculator {
             }
             // Check diagonal moves
             ChessPosition downRight = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()+1);
-            if (!downRight.outsideBounds() && board.getPiece(downRight) != null && board.getPiece(downRight).getTeamColor() != pieceColor)
-                validMoves.add(new ChessMove(myPosition, downRight, null));
+            if (!downRight.outsideBounds() && board.getPiece(downRight) != null && board.getPiece(downRight).getTeamColor() != pieceColor) {
+                // Check if piece can be promoted
+                if (downRight.getRow() == 1) {
+                    validMoves.add(new ChessMove(myPosition, downRight, ChessPiece.PieceType.BISHOP));
+                    validMoves.add(new ChessMove(myPosition, downRight, ChessPiece.PieceType.KNIGHT));
+                    validMoves.add(new ChessMove(myPosition, downRight, ChessPiece.PieceType.QUEEN));
+                    validMoves.add(new ChessMove(myPosition, downRight, ChessPiece.PieceType.ROOK));
+                } else
+                    validMoves.add(new ChessMove(myPosition, downRight, null));
+            }
             ChessPosition downLeft = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()-1);
-            if (!downRight.outsideBounds() && board.getPiece(downLeft) != null && board.getPiece(downLeft).getTeamColor() != pieceColor)
-                validMoves.add(new ChessMove(myPosition, downLeft, null));
+            if (!downRight.outsideBounds() && board.getPiece(downLeft) != null && board.getPiece(downLeft).getTeamColor() != pieceColor) {
+                // Check if piece can be promoted
+                if (downLeft.getRow() == 1) {
+                    validMoves.add(new ChessMove(myPosition, downLeft, ChessPiece.PieceType.BISHOP));
+                    validMoves.add(new ChessMove(myPosition, downLeft, ChessPiece.PieceType.KNIGHT));
+                    validMoves.add(new ChessMove(myPosition, downLeft, ChessPiece.PieceType.QUEEN));
+                    validMoves.add(new ChessMove(myPosition, downLeft, ChessPiece.PieceType.ROOK));
+                } else
+                    validMoves.add(new ChessMove(myPosition, downLeft, null));
+            }
         } else {
             // Check if piece can go up one
             ChessPosition upOne = new ChessPosition(myPosition.getRow()+1, myPosition.getColumn());
             if (board.getPiece(upOne) == null && !upOne.outsideBounds()) {
-                //Check if piece can be promoted
-                if (upOne.getRow() == 1)
-                    validMoves.add(new ChessMove(myPosition, upOne, null));
-                else {
-                    validMoves.add(new ChessMove(myPosition, upOne, null));
-                    // Check if piece can go up two
+                // Check if piece can be promoted
+                if (upOne.getRow() == 8) {
+                    validMoves.add(new ChessMove(myPosition, upOne, ChessPiece.PieceType.BISHOP));
+                    validMoves.add(new ChessMove(myPosition, upOne, ChessPiece.PieceType.KNIGHT));
+                    validMoves.add(new ChessMove(myPosition, upOne, ChessPiece.PieceType.QUEEN));
+                    validMoves.add(new ChessMove(myPosition, upOne, ChessPiece.PieceType.ROOK));
+                } else {
+                    validMoves.add(new ChessMove(myPosition, upOne, null));// Check if piece can go up two
                     ChessPosition upTwo = new ChessPosition(myPosition.getRow() + 2, myPosition.getColumn());
                     if (myPosition.getRow() == 2 && board.getPiece(upTwo) == null)
                         validMoves.add(new ChessMove(myPosition, upTwo, null));
@@ -49,11 +71,27 @@ public class PawnMovesCalculator extends MovesCalculator {
             }
             // Check diagonal moves
             ChessPosition upRight = new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()+1);
-            if (!upRight.outsideBounds() && board.getPiece(upRight) != null && board.getPiece(upRight).getTeamColor() != pieceColor)
-                validMoves.add(new ChessMove(myPosition, upRight, null));
+            if (!upRight.outsideBounds() && board.getPiece(upRight) != null && board.getPiece(upRight).getTeamColor() != pieceColor) {
+                // Check if piece can be promoted
+                if (upRight.getRow() == 8) {
+                    validMoves.add(new ChessMove(myPosition, upRight, ChessPiece.PieceType.BISHOP));
+                    validMoves.add(new ChessMove(myPosition, upRight, ChessPiece.PieceType.KNIGHT));
+                    validMoves.add(new ChessMove(myPosition, upRight, ChessPiece.PieceType.QUEEN));
+                    validMoves.add(new ChessMove(myPosition, upRight, ChessPiece.PieceType.ROOK));
+                } else
+                    validMoves.add(new ChessMove(myPosition, upRight, null));
+            }
             ChessPosition upLeft = new ChessPosition(myPosition.getRow()+1, myPosition.getColumn()-1);
-            if (!upRight.outsideBounds() && board.getPiece(upLeft) != null && board.getPiece(upLeft).getTeamColor() != pieceColor)
-                validMoves.add(new ChessMove(myPosition, upLeft, null));
+            if (!upLeft.outsideBounds() && board.getPiece(upLeft) != null && board.getPiece(upLeft).getTeamColor() != pieceColor) {
+                // Check if piece can be promoted
+                if (upLeft.getRow() == 8) {
+                    validMoves.add(new ChessMove(myPosition, upLeft, ChessPiece.PieceType.BISHOP));
+                    validMoves.add(new ChessMove(myPosition, upLeft, ChessPiece.PieceType.KNIGHT));
+                    validMoves.add(new ChessMove(myPosition, upLeft, ChessPiece.PieceType.QUEEN));
+                    validMoves.add(new ChessMove(myPosition, upLeft, ChessPiece.PieceType.ROOK));
+                } else
+                    validMoves.add(new ChessMove(myPosition, upLeft, null));
+            }
         }
 
         return validMoves;
