0cb7dd411629fe05cac0f36a64d6019d0df3cc83
root <root@LAPTOP-EFPRBT9A>
2024-10-22 15:36:41 -0600

    phase 3 start

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,30 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        Spark.get("/test", (Request req, Response res)->{
+            res.body("Hello");
+            return "hello";
+        });
+        Spark.post("/user", );
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
