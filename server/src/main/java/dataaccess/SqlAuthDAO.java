2f1829c3da73af38c4f8152d3ad714ed0a50360e
root <root@LAPTOP-EFPRBT9A>
2024-11-01 18:08:33 -0600

    SqlAuthDAO completed

--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -1,33 +1,98 @@
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class SqlAuthDAO implements AuthDAO {
     @Override
     public void clear() throws DataAccessException {
-
+        String sql = "DELETE FROM auth";
+        try (Connection connection = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error clearing auth table: " + e.getMessage());
+        }
     }
 
     @Override
     public String getUsername(String authToken) throws DataAccessException {
-        return "";
+        String sql = "SELECT username FROM auth WHERE auth_token = ?";
+        try (Connection connection = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, authToken);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getString("username");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error getting username from auth table: " + e.getMessage());
+        }
+        return null;
     }
 
     @Override
     public String createAuthToken(String username) throws DataAccessException {
-        return "";
+        String sql = "INSERT INTO auth (username, auth_token) VALUES (?, ?)";
+        try (Connection connection = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            String authToken = UUID.randomUUID().toString();
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, authToken);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error creating auth token: " + e.getMessage());
+        }
+        return null;
     }
 
     @Override
     public void deleteAuthToken(String authToken) throws DataAccessException {
+        String sql = "DELETE FROM auth WHERE auth_token = ?";
+        try (Connection connection = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, authToken);
 
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error deleting auth token: " + e.getMessage());
+        }
     }
 
     @Override
     public Integer getSize() throws DataAccessException {
+        String sql = "SELECT COUNT(*) FROM auth";
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error getting size from auth table: " + e.getMessage());
+        }
         return 0;
     }
 
     @Override
     public Boolean verifyAuthToken(String authToken) throws DataAccessException {
-        return null;
+        String sql = "SELECT 1 FROM auth WHERE auth_token = ? LIMIT 1";
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, authToken);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error verifying auth token: " + e.getMessage());
+        }
+        return false;
     }
 }
